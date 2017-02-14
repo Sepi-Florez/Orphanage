56c88edbdb46f7d01cfc62fa6c6b0ca619812a16
David Vink <david.vink@hotmail.com>
2017-02-14 13:18:03 +0100

    Added Presentation Project Folder

---
+++ Documents/Presentatie/TheseusPresentation/Assets/Camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public float h;
+    public float r;
+    public float speed;
+    public float t = 0;
+    public float tt;
+    void Update() {
+        Input_();
+    }
+    void Input_() {
+        if(Input.GetButtonDown("Fire1")) {
+            t = 0;
+            StartCoroutine(NextFrame());
+
+        }
+    }
+    IEnumerator NextFrame() {
+        t++;
+        if (t == tt) {
+
+        }
+
+            yield return new WaitForSeconds(speed);
+
+    }
+}
