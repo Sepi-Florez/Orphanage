6da139cee5d76fd6b94ce9b60f48df2f2d984e7e
David Vink <david.vink@hotmail.com>
2017-03-07 13:20:22 +0100

    XML Item database working

--- ProjectTheseus/Assets/_Scripts/_FrontEnd/_UI/InventoryManager.cs
+++ ProjectTheseus/Assets/_Scripts/_FrontEnd/_UI/InventoryManager.cs
@@ -1,32 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 using System;
 using System.Xml;
 using System.Xml.Serialization;
 [Serializable]
 public class InventoryManager : MonoBehaviour {
-
-    public List<Item>dbList= new List<Item>();
+    public string dataPath;
+    public ItemDatabase dbList;
     public List<Item>inventory = new List<Item>();
 	void Awake () {
-        FileStream stream = new FileStream(Application.dataPath + "/XML/ItemDatabase.xml", FileMode.Open);
-        XmlSerializer reader = new XmlSerializer(typeof(ItemDatabase));
-        ItemDatabase a = reader.Deserialize(stream) as ItemDatabase ;
-        stream.Close();
-        a.itemList = dbList;
+        if (File.Exists(Application.dataPath + dataPath)) {
+            FileStream stream = new FileStream(Application.dataPath + dataPath, FileMode.Open);
+            XmlSerializer reader = new XmlSerializer(typeof(ItemDatabase));
+            ItemDatabase a = reader.Deserialize(stream) as ItemDatabase;
+            stream.Close();
+            a = dbList;
+            print("Loaded Database");
+        }
+        else {
+            dbList = new ItemDatabase();
+            print("Did not load Database");
+        }
     }
 
 	void Update () {
 
 	}
     public void AddItem (int ID, int count) {
-        inventory.Add(dbList[ID]);
+        inventory.Add(dbList.itemList[ID]);
         inventory[0].count = count;
         Organize();
     }
     public void Organize() {
+
+    }
+    public void CreateItem(Transform creator) {
+        if (creator.GetChild(0).transform.GetChild(2).GetComponent<Text>().text != null) {
+            Item newItem = new Item();
+            newItem.itemName = creator.GetChild(0).transform.GetChild(2).GetComponent<Text>().text;
+            newItem.itemID = dbList.itemList.Count;
+            dbList.itemList.Add(newItem);
+            XmlSerializer writer = new XmlSerializer(typeof(ItemDatabase));
+            FileStream stream = new FileStream(Application.dataPath + dataPath, FileMode.Create);
+            writer.Serialize(stream, dbList);
+            stream.Close();
+            print("AddedItem");
+        }
+        else {
+            print("did not add item");
+        }
 
     }
 }
