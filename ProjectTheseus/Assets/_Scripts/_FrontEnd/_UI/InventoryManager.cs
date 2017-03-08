d48b55c5494fba9db08506669223e720af7f77bf
David Vink <david.vink@hotmail.com>
2017-03-08 17:01:03 +0100

    Fixed inventory database visualization.
    Theres one minor change needed that includes making seperate lists for
    each category

--- ProjectTheseus/Assets/_Scripts/_FrontEnd/_UI/InventoryManager.cs
+++ ProjectTheseus/Assets/_Scripts/_FrontEnd/_UI/InventoryManager.cs
@@ -11,6 +11,9 @@ public class InventoryManager : MonoBehaviour {
     public string dataPath;
     public ItemDatabase dbList;
     Transform inventoryObj;
+    Transform equipObj;
+    Transform materialObj;
+
     public GameObject ItemPref;
     public List<Item>inventory = new List<Item>();
 	void Awake () {
@@ -27,33 +30,79 @@ public class InventoryManager : MonoBehaviour {
             print("Did not load Database");
         }
         inventoryObj = GameObject.FindGameObjectWithTag("Inventory").transform;
+        equipObj = GameObject.FindGameObjectWithTag("equipObject").transform;
+        materialObj = GameObject.FindGameObjectWithTag("materialObject").transform;
     }
 
 	void Update () {
         if (Input.GetButtonDown("Jump")) {
             AddItem(0, 2);
         }
+        if (Input.GetButtonDown("Fire1")) {
+            AddItem(1, 10);
+        }
+        if (Input.GetButtonDown("Fire2")) {
+            AddItem(2, 10);
+        }
 	}
     public void AddItem (int ID, int count) {
         if (dbList.itemList.Count > ID) {
-            inventory.Add(dbList.itemList[ID]);
-            inventory[0].count = count;
-            Visualize(dbList.itemList[ID],0); 
-            Organize();
+            switch (dbList.itemList[ID].category) {
+                case 0:
+                    bool dub = true;
+                    for(int a = 0; inventory.Count > a; a++) {
+
+                        if(inventory[a].itemName == dbList.itemList[ID].itemName) {
+                            inventory[a].count += count;
+                            Refresh(a);
+                            dub = false;
+                        }
+                        
+                    }
+                    if (dub) {
+                        CreateAdd(ID, count);
+                    }
+                    break;
+                case 1:
+                    CreateAdd(ID, count);
+                    break;
+            }
+
         }
     }
+    public void CreateAdd(int ID, int count) {
+        inventory.Add(dbList.itemList[ID]);
+        inventory[0].count = count;
+        Visualize(dbList.itemList[ID]);
+    }
+
     void Organize() {
 
     }
-    public void Visualize(Item newItem, int option) {
+    public void Visualize(Item newItem) {
+        print("Visualizing");
         Transform insItem = (Transform)Instantiate(ItemPref, inventoryObj.position, Quaternion.identity).transform;
-        insItem.transform.SetParent(inventoryObj.GetChild(newItem.category));
-        insItem.GetChild(0).GetComponent<Text>().text = newItem.itemName + newItem.count;
+        insItem.GetChild(0).GetComponent<Text>().text = newItem.itemName;
+        switch (newItem.category) {
+            case 0:
+                insItem.transform.SetParent(equipObj);
+                insItem.transform.GetChild(2).GetComponent<Text>().text = inventory[0].count.ToString();
+                break;
+            case 1:
+                insItem.transform.SetParent(materialObj);
+                break;
+        }
+        Organize();
+
+    }
+    public void Refresh(int num) {
+        equipObj.GetChild(num).transform.GetChild(2).GetComponent<Text>().text = inventory[num].count.ToString();
     }
     public void CreateItem(Transform creator) {
         if (creator.GetChild(0).transform.GetChild(2).GetComponent<Text>().text != null) {
             Item newItem = new Item();
             newItem.itemName = creator.GetChild(0).transform.GetChild(2).GetComponent<Text>().text;
+            newItem.category = Convert.ToInt32(creator.GetChild(1).transform.GetChild(2).GetComponent<Text>().text);
             newItem.itemID = dbList.itemList.Count;
             dbList.itemList.Add(newItem);
             XmlSerializer writer = new XmlSerializer(typeof(ItemDatabase));
