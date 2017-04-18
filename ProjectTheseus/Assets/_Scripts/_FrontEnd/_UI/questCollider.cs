60c525a7d328d94a3a9fe49a18619ffd3296c4eb
David Vink <david.vink@hotmail.com>
2017-04-18 11:12:41 +0200

    Minor changes

--- ProjectTheseus/Assets/_Scripts/_FrontEnd/_UI/questCollider.cs
+++ ProjectTheseus/Assets/_Scripts/_FrontEnd/_UI/questCollider.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Quie : MonoBehaviour {
+public class questCollider : MonoBehaviour {
+    public int questID;
     private void OnTriggerEnter(Collider intruder) { 
-        if(intruder.transform.tag == "Player") { 
-            if(QuestManager.thisManager.QuestCheck)
+        if(intruder.transform.tag == "Player") {
+            if (QuestManager.thisManager.QuestCheck(questID)) {
+
+            }
         }
     }
 }
