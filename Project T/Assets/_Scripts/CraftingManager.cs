9c8c6724b3d518d0e132985986a0b25907d8ec77
David Vink <david.vink@hotmail.com>
2017-05-09 16:42:45 +0200

    Crafting UI work

---
+++ Project T/Assets/_Scripts/CraftingManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingManager : MonoBehaviour {
+    public GameObject recipePref;
+    public void Start() {
+        AddRecipe(1);
+    }
+    public void AddRecipe(int ItemID) {
+        Transform newRecipe = Instantiate(recipePref, Vector3.zero, Quaternion.identity).transform;
+        newRecipe.SetParent(GameObject.FindGameObjectWithTag("CraftingContent").transform);
+        InventoryManager.thisManager.helpArrange(newRecipe);
+        newRecipe.GetComponent<RecipeButton>().FillValues(DataBaseManager.thisManager.GetItem(ItemID));
+    }
+}
---
+++ Project T/Assets/_Scripts/RecipeButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeButton : MonoBehaviour {
+    int thisID;
+
+    public void FillValues(Item item) {
+        thisID = item.ID;
+        transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = Resources.Load<Sprite>(item.spritePath);
+        transform.GetChild(0).GetChild(1).GetComponent<Text>().text = item.itemName;
+        Transform mats = transform.GetChild(1);
+        for(int i = 0; i == item.comps.Length; i++) {
+            Item newItem = DataBaseManager.thisManager.GetItem(item.comps[i]);
+            mats.GetChild(i).GetChild(0).GetComponent<Image>().sprite = Resources.Load<Sprite>(newItem.spritePath);
+            mats.GetChild(i).GetChild(1).GetComponent<Text>().text = newItem.itemName;
+            switch (newItem.category) {
+                case 1:
+                    Consumable newCon = InventoryManager.thisManager.Search(newItem.ID) as Consumable;
+                    mats.GetChild(i).GetChild(2).GetComponent<Text>().text = newCon.count.ToString();
+                    break;
+                case 2:
+                    CraftingObject newCra = InventoryManager.thisManager.Search(newItem.ID) as CraftingObject;
+                    mats.GetChild(i).GetChild(2).GetComponent<Text>().text = newCra.count.ToString();
+                    break;
+
+            }
+            mats.GetChild(i).GetChild(3).GetComponent<Text>().text = item.compsc[i].ToString();
+        }
+        //if(item.comps.Length < mats.childCount) {
+        //    for (int a = 5 - item.comps.Length; a == 0; a--) {
+        //        Destroy(mats.GetChild(mats.childCount - 1));
+        //    }
+        //}
+    }
+    public void UpdateValues() {
+
+    }
+}
