51e37099350f9749ce310407408b83f48bca8ebf
DeesV <dees.veen@ehldeventer.nl>
2017-05-16 13:00:46 +0200

    BunnyAIStart
    Begin van de bunnyAI, BunnyScene + _AI map aangemaakt samen met een
    BunnyBehaviour script

---
+++ Project T/Assets/_Scripts/_FrontEnd/_AI/BunnyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyBehaveiour : MonoBehaviour {
+
+    public bool isActive;
+    public float movementSpeed;
+    public GameObject[] rabbitHoles;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
