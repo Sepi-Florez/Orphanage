671afd9660562ffd3aa19431b12be168e4859249
DeesV <dees.veen@ehldeventer.nl>
2017-05-18 11:51:16 +0200

    BunnyBehaviour Movement
    De Bunny heeft nu een compleet eigen movement system, nu start ik aan
    een playerdetection

--- Project T/Assets/_Scripts/_FrontEnd/_AI/BunnyBehaviour.cs
+++ Project T/Assets/_Scripts/_FrontEnd/_AI/BunnyBehaviour.cs
@@ -7,16 +7,55 @@ public class BunnyBehaviour : MonoBehaviour {
 
     public Transform target;
     NavMeshAgent agent;
+    float waitTime;
+    bool inWaitState = false;
+
+    public float minWaitTime;
+    public float maxWaitTime;
+
+    public float range = 10.0f;
+    bool randomPoint(Vector3 center, float range, out Vector3 result) {
+        for (int i = 0; i < 2; i++) {
+            Vector3 randomPoint = center + Random.insideUnitSphere * range;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) {
+                result = hit.position;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        //StartCoroutine(PickPoint());
+        return false;
+    }
 
     // Use this for initialization
     void Start() {
         agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.position);
     }
 
     // Update is called once per frame
     void Update() {
-        
-        agent.SetDestination(target.position);
+        if (agent.remainingDistance <= agent.stoppingDistance) {
+            if (inWaitState == false) {
+                StartCoroutine(PickPoint());
+            }
+        }
+    }
+
+    IEnumerator PickPoint() {
+        Vector3 point;
+        if (randomPoint(transform.position, range, out point)) {
+            print(point);
+            inWaitState = true;
+            waitTime = Random.Range(minWaitTime, maxWaitTime);
+            yield return new WaitForSeconds(waitTime);
+            target.position = point;
+            agent.SetDestination(target.position);
+            Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+            inWaitState = false;
+        }
+        yield return point;
     }
 }
 
