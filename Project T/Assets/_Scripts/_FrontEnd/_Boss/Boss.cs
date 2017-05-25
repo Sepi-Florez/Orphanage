7684015a5e633c7896bf8c0f7d4746e18880cd64
David V <david.vink@hotmail.com>
2017-05-25 17:59:31 +0200

    Working on Boss behaviour, navmesh finished.

---
+++ Project T/Assets/_Scripts/_FrontEnd/_Boss/Boss.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+    UnityEngine.AI.NavMeshAgent agent;
+    public Transform i;
+    Transform player;
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        //StartCoroutine(Turn());
+    }
+    void Update() {
+
+    }
+    IEnumerator Turn() {
+        yield return new WaitForSeconds(0);
+    }
+}
