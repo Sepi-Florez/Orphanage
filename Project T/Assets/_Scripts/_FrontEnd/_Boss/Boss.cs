1603f6fcb98a212fae8d777c6b27d46598778f9c
David V <david.vink@hotmail.com>
2017-06-19 19:33:12 +0200

    Boss Work

--- Project T/Assets/_Scripts/_FrontEnd/_Boss/Boss.cs
+++ Project T/Assets/_Scripts/_FrontEnd/_Boss/Boss.cs
@@ -7,9 +7,12 @@ public class Boss : MonoBehaviour {
     Animator anim;
 
     Transform player;
+    Vector3 lastPos;
 
     delegate void Action();
     Action bossAction;
+
+    public Transform weapon;
 
     //Health
     public int healh;
@@ -17,9 +20,16 @@ public class Boss : MonoBehaviour {
 
     //floats
     public float minAngle;
+    public float chargeSpeed;
+    public float weaponRadius;
 
     //Bools
-    public bool looking = true; 
+    public bool looking = true;
+    public bool lookFollow;
+    public bool charge = false;
+
+    //Coroutines
+    public Coroutine looker;
 
     //Fills in some variables
 	void Start () {
@@ -35,9 +45,10 @@ public class Boss : MonoBehaviour {
         print("Deciding");
         float dist = Vector3.Distance(transform.position, player.position);
         float stopDist;
-        if (dist > 30) {
+        if (dist > 25) {
             bossAction += BullCharge;
             stopDist = dist;
+            charge = true;
         }
         else {
             int r = Random.Range(0, 2);
@@ -55,50 +66,76 @@ public class Boss : MonoBehaviour {
     // Makes the boss look at the player when in the 
     IEnumerator LookForPlayer() {
         while (looking) {
+
             Vector3 ppos = new Vector3(player.position.x, transform.position.y, player.position.z);
             if (Vector3.Angle(transform.forward, ppos - transform.position) < minAngle) {
-                anim.SetBool("Walking", true);
-                anim.SetBool("Looking", false);
-                print("Player in vision");
-                transform.LookAt(ppos);
-                Move();
                 if (agent.isStopped) {
                     agent.isStopped = false;
                     DecideAction();
                 }
                 else if (agent.remainingDistance - agent.stoppingDistance <= 0) {
                     bossAction();
+
                     anim.SetBool("Walking", false);
                     agent.isStopped = true;
                     looking = false;
+                    break;
                 }
+                anim.SetBool("Walking", true);
+                transform.LookAt(ppos);
+                Move();
             }
             else {
                 if (!agent.isStopped)
                     agent.isStopped = true;
+                
 
-                anim.SetBool("Looking", true);
                 Quaternion rotation = Quaternion.LookRotation(ppos - transform.position);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 1);
-                print("is turning to");
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
             }
             yield return new WaitForSeconds(0.01f);
         }
     }
     void StartLooking() {
         looking = true;
-        anim.SetBool("Looking", true);
-        StartCoroutine(LookForPlayer());
-        print("Start");
+        looker = StartCoroutine(LookForPlayer());
+        print("Start looking");
     }
-    //IEnumerator AttackFollow() {
-    //Moves the boss towards the player.
+    IEnumerator AttackFollow() {
+        while (lookFollow) {
+            Vector3 ppos = new Vector3(player.position.x, transform.position.y, player.position.z);
+            if (Vector3.Angle(transform.forward, ppos - transform.position) < minAngle) {
+                transform.LookAt(player);
+                lastPos = player.position;
+                
+            }
+            yield return new WaitForSeconds(0.01f);
+        }    
+    }
+    IEnumerator Charge() {
+        while (charge) {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position,transform.forward,out hit, 3)) {
+                if(hit.transform.tag == "Player") {
+                    charge = false;
+                }
+                charge = false;
+                break;
+            }
+            transform.Translate(player.position * chargeSpeed);
+            print(lastPos);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
     void Move() {
         print("moving");
         agent.SetDestination(player.position);
     }
     void Overhead() {
         print("Overhead");
+        lookFollow = true;
+        StartCoroutine(AttackFollow());
         anim.SetTrigger("Overhead");
         bossAction -= Overhead;
     }
@@ -110,6 +147,27 @@ public class Boss : MonoBehaviour {
     }
     void BullCharge() {
         print("CHAAAAAAAAAAARGE");
+        anim.SetTrigger("Charge");
+        lookFollow = true;
+        StartCoroutine(AttackFollow());
         bossAction -= BullCharge;
+    }
+    void EventCall(int i) {
+        lookFollow = false;
+        switch (i) {
+            case 1:
+                charge = true;
+                StartCoroutine(Charge());
+                break;
+        }
+
+    }
+    void HitCheck() {
+        RaycastHit hit;
+        if(Physics.SphereCast(weapon.position,weaponRadius,transform.forward, out hit)) {
+            if(hit.transform.tag == "Player") {
+                print("Player");
+            }
+        }
     }
 }
