b2fcc35fa7faf3050b5f363947a729a5440c234b
DeesV <dees.veen@ehldeventer.nl>
2017-06-21 19:56:03 +0200

    Start SoundManager

---
+++ Project T/Assets/_Scripts/_FrontEnd/_Sound/SoundManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public List<AudioClip> useableSounds = new List<AudioClip>();
+
+	// Use this for initialization
+	void Awake () {
+		//play the all-around music now, moet nog wel loopen 
+	}
+	
+}
