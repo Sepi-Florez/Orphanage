860fee3447e981b4c66e8def457712cc5257ea05
DeesV <dees.veen@ehldeventer.nl>
2017-06-23 12:58:34 +0200

    SoundManager_Works_WorkingOnImprovement

---
+++ Project T/Assets/_Scripts/_FrontEnd/_Sound/S2SSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S2SSound : MonoBehaviour {
+
+    public SoundManager thisManager;
+
+	// Use this for initialization
+	void Start () {
+        thisManager = GetComponent<SoundManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+            //print("DitDoetHijWel");
+            thisManager.audioNumber = 11;
+            //print("DitDoetHijOok");
+            thisManager.SoundLister();
+        }
+	}
+}
--- Project T/Assets/_Scripts/_FrontEnd/_Sound/SoundManager.cs
+++ Project T/Assets/_Scripts/_FrontEnd/_Sound/SoundManager.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour {
 
     public List<AudioClip> useableSounds = new List<AudioClip>();
+    public int audioNumber;
+    public AudioSource gOAS;
 
-	// Use this for initialization
-	void Awake () {
-		//play the all-around music now, moet nog wel loopen 
-	}
-	
+    void Awake() {
+        gOAS = transform.GetComponent<AudioSource>();
+        /*foreach (AudioClip a in useableSounds) {
+            audioNumber = useableSounds.IndexOf(a);
+            print(audioNumber);
+            Debug.Log(audioNumber);
+        }*/
+
+        // Use this for initialization
+
+        // Use this for initialization
+
+       
+    }
+    public void SoundLister () {
+        print("SoundListerActivated!");
+        if (audioNumber <= useableSounds.Count) {
+            gOAS.PlayOneShot(useableSounds[audioNumber]);
+        }
+        else {
+            print("SoundRequest out of range! The audioNumber you are requesting is not available");
+        }
+    }
+
 }
